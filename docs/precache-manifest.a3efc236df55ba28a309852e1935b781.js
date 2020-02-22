3f1ed25a3482e54b978b721c2fca155c114f59df
sutej <sutej@kritivity.com>
2020-02-22 21:37:03 +0530

    initial commit

---
+++ docs/precache-manifest.a3efc236df55ba28a309852e1935b781.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7ec21aae33cbaf5147",
+    "url": "/css/about.2968e387.css"
+  },
+  {
+    "revision": "29f503248a5c4867db39",
+    "url": "/css/app.41b39501.css"
+  },
+  {
+    "revision": "0ef99ec04187b7d9dba4",
+    "url": "/css/chunk-d6741eec.a29b5da2.css"
+  },
+  {
+    "revision": "725072d58384fd66e5c395f549ee3795",
+    "url": "/fonts/Mochary.725072d5.ttf"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/fonts/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3972996d9e03a4df453ada25dc7bde25",
+    "url": "/img/shubham (1).3972996d.jpg"
+  },
+  {
+    "revision": "35140b6a6df29e83a98a7a8e22f3437d",
+    "url": "/img/shubham (10).35140b6a.jpg"
+  },
+  {
+    "revision": "4d6db0e2244527d05f2b5bb56fcb56a3",
+    "url": "/img/shubham (11).4d6db0e2.jpg"
+  },
+  {
+    "revision": "fc753eee111dff81125f039c00dadfee",
+    "url": "/img/shubham (12).fc753eee.jpg"
+  },
+  {
+    "revision": "0dbd6d4c3e14d687b665319770223a28",
+    "url": "/img/shubham (13).0dbd6d4c.jpg"
+  },
+  {
+    "revision": "cf361be6c7a2afbbf0724eeb8e15b543",
+    "url": "/img/shubham (14).cf361be6.jpg"
+  },
+  {
+    "revision": "51b507ac2f8aa1e88a284e5ede26847e",
+    "url": "/img/shubham (15).51b507ac.jpg"
+  },
+  {
+    "revision": "8a7eb2c5336357d80dfa52405f017af4",
+    "url": "/img/shubham (16).8a7eb2c5.jpg"
+  },
+  {
+    "revision": "bff9e19c068e99e294ec408bb97b5136",
+    "url": "/img/shubham (17).bff9e19c.jpg"
+  },
+  {
+    "revision": "45daadfb5059c3c5fb052d206d399571",
+    "url": "/img/shubham (18).45daadfb.jpg"
+  },
+  {
+    "revision": "336bdc177bce1de88981577d271bcd52",
+    "url": "/img/shubham (19).336bdc17.jpg"
+  },
+  {
+    "revision": "5210f22620d9a2f9491b42dcca517e1f",
+    "url": "/img/shubham (2).5210f226.jpg"
+  },
+  {
+    "revision": "2102c2c765d8ef287068e38f268a71ed",
+    "url": "/img/shubham (20).2102c2c7.jpg"
+  },
+  {
+    "revision": "d10b01ec7b3f831a6ed06f02f97de85d",
+    "url": "/img/shubham (21).d10b01ec.jpg"
+  },
+  {
+    "revision": "8c97ba1fa9cdda6245e5bb0b082e3673",
+    "url": "/img/shubham (22).8c97ba1f.jpg"
+  },
+  {
+    "revision": "4a6748cd6b024609bbc9ad00c85d4920",
+    "url": "/img/shubham (23).4a6748cd.jpg"
+  },
+  {
+    "revision": "f919533d2a628611a516441faca740dc",
+    "url": "/img/shubham (24).f919533d.jpg"
+  },
+  {
+    "revision": "5dbbe0b5f8be7e5d81d5a65cb16f7c34",
+    "url": "/img/shubham (25).5dbbe0b5.jpg"
+  },
+  {
+    "revision": "44dd881fe51a9b4a9da22a3e1d37c3ce",
+    "url": "/img/shubham (26).44dd881f.jpg"
+  },
+  {
+    "revision": "d5eaaebff521ac5fe80cc7210104ec84",
+    "url": "/img/shubham (27).d5eaaebf.jpg"
+  },
+  {
+    "revision": "eacb88b3418bae5ce41ecc27e1c46d73",
+    "url": "/img/shubham (28).eacb88b3.jpg"
+  },
+  {
+    "revision": "e2220bced6a5fc1a08b0134b46dd374f",
+    "url": "/img/shubham (29).e2220bce.jpg"
+  },
+  {
+    "revision": "675272fc25893d1f73b2f25f85444446",
+    "url": "/img/shubham (3).675272fc.jpg"
+  },
+  {
+    "revision": "e22a153469eb3e9131118bacd934e57f",
+    "url": "/img/shubham (30).e22a1534.jpg"
+  },
+  {
+    "revision": "d5126d24c7858e4a1505d9ef510d8582",
+    "url": "/img/shubham (31).d5126d24.jpg"
+  },
+  {
+    "revision": "fed6fc76e24bc0c4b5b4ed0b20058fd4",
+    "url": "/img/shubham (32).fed6fc76.jpg"
+  },
+  {
+    "revision": "89f95d552738bdd790a79cabe9b9a07c",
+    "url": "/img/shubham (4).89f95d55.jpg"
+  },
+  {
+    "revision": "545d65a0beb59b88445506b3fea32cff",
+    "url": "/img/shubham (5).545d65a0.jpg"
+  },
+  {
+    "revision": "2cf98be21221708e6aa2f738627f6117",
+    "url": "/img/shubham (6).2cf98be2.jpg"
+  },
+  {
+    "revision": "cbca3a4dd8f5f2143d5b976c9c4253db",
+    "url": "/img/shubham (7).cbca3a4d.jpg"
+  },
+  {
+    "revision": "109a7bd0c46a09ca7b9f5eef2f586075",
+    "url": "/img/shubham (8).109a7bd0.jpg"
+  },
+  {
+    "revision": "ee2fe736fe6fdb13e9a2859b48825b6d",
+    "url": "/img/shubham (9).ee2fe736.jpg"
+  },
+  {
+    "revision": "9c2ad15182f7f061c81cf088beaa4546",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a7ec21aae33cbaf5147",
+    "url": "/js/about.fe776036.js"
+  },
+  {
+    "revision": "29f503248a5c4867db39",
+    "url": "/js/app.e2c4bd3e.js"
+  },
+  {
+    "revision": "0ef99ec04187b7d9dba4",
+    "url": "/js/chunk-d6741eec.a47d4a14.js"
+  },
+  {
+    "revision": "e1454f040b22c84893db",
+    "url": "/js/chunk-vendors.e1dd45ab.js"
+  },
+  {
+    "revision": "727a39272fcd05507692cdb6e1d72403",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
